f759c3badf1f63d5184629fbff425ed4b97648f6
STORVBOX Team <storvbox@example.com>
2025-10-25 21:15:44 +0000

    auto-commit for 0e79795d-e8f0-4ef8-beaa-7dcf8eb7a6b4

--- app/contact/page.js
+++ app/contact/page.js
@@ -322,13 +322,14 @@ export default function ContactPage() {
       <section className="py-0">
         <div className="w-full h-96 bg-gray-200">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.5982947935995!2d23.289634315473445!3d42.67556797916493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDQwJzMyLjAiTiAyM8KwMTcnMjguMCJF!5e0!3m2!1sen!2sbg!4v1234567890123!5m2!1sen!2sbg"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.1234567890123!2d23.348!3d42.686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sJ9PX%2B3M%20Sofia!5e0!3m2!1sen!2sbg!4v1234567890123!5m2!1sen!2sbg"
             width="100%"
             height="100%"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
+            title="Storybox Location in Sofia"
           ></iframe>
         </div>
       </section>
