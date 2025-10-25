07c70b703a6c8267a497c7f57bb05a08bf79f508
STORVBOX Team <storvbox@example.com>
2025-10-25 22:08:27 +0000

    auto-commit for f9c995ba-c548-4265-850c-74a017914e5b

---
+++ sentry.edge.config.js
@@ -0,0 +1,14 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://7c59c7c02de414fd4a3faa931a185232@o4510252345786369.ingest.de.sentry.io/4510252364398672',
+  
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+  
+  // Environment
+  environment: process.env.NODE_ENV || 'development',
+})
