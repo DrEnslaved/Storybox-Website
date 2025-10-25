c592932809af92f6fdb5cba56e1e2498d5d59850
STORVBOX Team <storvbox@example.com>
2025-10-25 22:12:22 +0000

    auto-commit for f4370e52-763e-40be-86aa-dacc0a91976c

---
+++ jest.setup.js
@@ -0,0 +1,35 @@
+import '@testing-library/jest-dom'
+
+// Mock Next.js router
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+      pathname: '/',
+      query: {},
+      asPath: '/',
+    }
+  },
+  usePathname() {
+    return '/'
+  },
+  useSearchParams() {
+    return new URLSearchParams()
+  },
+}))
+
+// Mock Next.js Image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />
+  },
+}))
+
+// Mock environment variables
+process.env.NEXT_PUBLIC_BASE_URL = 'http://localhost:3000'
+process.env.MONGO_URL = 'mongodb://localhost:27017/test'
