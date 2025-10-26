6c435bae8ef6a99da7b15be3f73238b0190083f7
STORVBOX Team <storvbox@example.com>
2025-10-26 20:26:31 +0000

    auto-commit for 97fa1bba-f7e5-478e-8010-1a5ffd5e6c62

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Storybox CMS',
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: 'yhhlq588',
+  dataset: 'sbxdataset',
 
   plugins: [
     deskTool(),
