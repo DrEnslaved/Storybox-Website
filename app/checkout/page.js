281409c260f90015a7f6a5bb61a2255ef34ee34a
STORVBOX Team <storvbox@example.com>
2025-10-26 22:11:13 +0000

    auto-commit for 61336f0c-2130-4574-bfff-d7b316b28d40

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useAuth } from '@/contexts/AuthContext'
-import { useCart } from '@/contexts/SimpleCartContext'
+import { useCart } from '@/contexts/MedusaCartContext'
 import { CreditCard, MapPin, Package, ArrowLeft, Loader2 } from 'lucide-react'
 
 export default function CheckoutPage() {
